6ac51a2136a88fbba3568778fa3f0f9f52257921
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-05-30 16:32:59 -0700

    updated hardware plugin

--- OceanOptics_hardware.py
+++ OceanOptics_hardware.py
@@ -4,22 +4,19 @@ import seabreeze.spectrometers as sb
 class OceanOpticsHW(HardwareComponent):
     
     ## Define name of this hardware plug-in
-    name = 'oceanoptics_hardware'
+
     
     def setup(self):
         # Define your hardware settings here.
         # These settings will be displayed in the GUI and auto-saved with data files
+        self.name = 'oceanoptics_hardware'
         self.settings.New('intg_time', dtype=int, unit='us', initial=3000, vmin=3000)
         self.settings.New('correct_dark_counts', dtype=bool, initial=True)
 
     def connect(self):
         # Open connection to the device:
-        if self.spec is None:
-            devices = sb.list_devices()
-            self.spec = sb.Spectrometer(devices[0])
-            self.ui.status_textBrowser.append("Ocean Optics Device Connected!\n\n Device:\n\n"+str(sb.list_devices()[0]))
-        else:
-            self.ui.status_textBrowser.append("Already Connected")
+        devices = sb.list_devices()
+        self.spec = sb.Spectrometer(devices[0])
 
         #Connect settings to hardware:
         self.spec.settings.intg_time.connect_to_hardware(
@@ -34,7 +31,6 @@ class OceanOpticsHW(HardwareComponent):
         self.settings.disconnect_all_from_hardware()
         if self.spec is not None:
             self.spec.close()
-            self.ui.status_textBrowser.append("Ocean Optics Device Disconnected")
             del self.spec
             self.spec = None
 
