e699b7041d16c4ebb8cdfdf7e1e947af936d41fd
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-06-04 14:40:21 -0700

    updated ui

--- OceanOptics_measurement.py
+++ OceanOptics_measurement.py
@@ -2,6 +2,7 @@ from ScopeFoundry import Measurement
 from ScopeFoundry.helper_funcs import sibling_path, load_qt_ui_file
 from ScopeFoundry import h5_io
 import pyqtgraph as pg
+from PyQt5 import *
 import numpy as np
 import time
 import seabreeze.spectrometers as sb
