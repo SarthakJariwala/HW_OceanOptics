8cb2e3c0406f87cbe3db438946a37b639590c673
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-06-04 14:15:01 -0700

    restructured measurement, updated ui

--- OceanOptics_measurement.py
+++ OceanOptics_measurement.py
@@ -31,13 +31,11 @@ class OceanOpticsMeasure(Measurement):
 		# Measurement Specific Settings
 		# This setting allows the option to save data to an h5 data file during a run
 		# All settings are automatically added to the Microscope user interface
-		self.settings.New('save_h5', dtype=bool, initial=True)
-		self.settings.New('scans_to_avg', dtype=int, initial=0)
 		self.settings.New('save_every_spec', dtype=bool, initial=False)
+		self.settings.New('scans_to_avg', dtype=int, initial=0)
 
 		# Define how often to update display during a run
 		self.display_update_period = 0.1 
-		self.settings.New('sampling_period', dtype=float, initial = self.display_update_period)
 		
 		# Create empty numpy array to serve as a buffer for the acquired data
 		self.buffer = np.zeros(120, dtype=float)
@@ -62,7 +60,11 @@ class OceanOpticsMeasure(Measurement):
 		self.ui.start_pushButton.clicked.connect(self.start)
 		self.ui.interrupt_pushButton.clicked.connect(self.interrupt)
 		self.ui.saveSingle_pushButton.clicked.connect(self.save_single_spec)
-		self.settings.save_h5.connect_to_widget(self.ui.save_h5_checkBox)
+		
+		self.settings.save_every_spec.connect_to_widget(self.ui.save_every_spec_checkBox)
+		self.spec_hw.settings.correct_dark_counts.connect_to_widget(self.ui.correct_dark_counts_checkBox)
+		self.spec_hw.settings.intg_time.connect_to_widget(self.ui.intg_time_spinBox)
+
 		# Set up pyqtgraph graph_layout in the UI
 		self.graph_layout=pg.GraphicsLayoutWidget()
 		self.ui.plot_groupBox.layout().addWidget(self.graph_layout)
@@ -76,17 +78,13 @@ class OceanOpticsMeasure(Measurement):
 		self.optimize_plot_line = self.plot.plot([0])
 
 	def update_display(self):
+		"""
+        Displays (plots) the numpy array self.buffer. 
+        This function runs repeatedly and automatically during the measurement run.
+        its update frequency is defined by self.display_update_period
+        """
 		if hasattr(self, 'spec'):
-			self._read_spectrometer()
-			self.save_array[:,1] = self.y
-			
 			self.ui.plot.plot(self.spec.wavelengths(), self.y, pen='r', clear=True)
-			
-			if self.ui.save_every_spec_checkBox.isChecked():
-				self.save_array[:,0] = self.spec.wavelengths()
-				np.savetxt(self.app.settings['save_dir']+"/"+self.app.settings['sample']+str(self.point_counter)+".txt", self.save_array, fmt = '%.5f', header = 'Wavelength (nm), Intensity (counts)', delimiter = ' ')
-			self.point_counter += 1
-			pg.QtGui.QApplication.processEvents()
 
 	def run(self):
 		"""
@@ -95,71 +93,22 @@ class OceanOpticsMeasure(Measurement):
 		focus on data acquisition.
 		"""
 		self.spec = self.spec_hw.spec
-		# first, create a data file
-		if self.settings['save_h5']:
-			# if enabled will create an HDF5 file with the plotted data
-			# first we create an H5 file (by default autosaved to app.settings['save_dir']
-			# This stores all the hardware and app meta-data in the H5 file
-			self.h5file = h5_io.h5_base_file(app=self.app, measurement=self)
-			
-			# create a measurement H5 group (folder) within self.h5file
-			# This stores all the measurement meta-data in this group
-			self.h5_group = h5_io.h5_create_measurement_group(measurement=self, h5group=self.h5file)
-			
-			# create an h5 dataset to store the data
-			self.buffer_h5 = self.h5_group.create_dataset(name  = 'buffer', 
-														  shape = self.buffer.shape,
-														  dtype = self.buffer.dtype)
-		
-		# We use a try/finally block, so that if anything goes wrong during a measurement,
-		# the finally block can clean things up, e.g. close the data file object.
-		try:
-			i = 0
-			
-			# Will run forever until interrupt is called.
-			while not self.interrupt_measurement_called:
-				i %= len(self.buffer)
-				
-				# Set progress bar percentage complete
-				self.settings['progress'] = i * 100./len(self.buffer)
-				
-				# Fills the buffer with sine wave readings from func_gen Hardware
-				self.buffer[i] = self.app.hardware['oceanoptics'].read_from_hardware()
-				
-				if self.settings['save_h5']:
-					# if we are saving data to disk, copy data to H5 dataset
-					self.buffer_h5[i] = self.buffer[i]
-					# flush H5
-					self.h5file.flush()
-				
-				# wait between readings.
-				# We will use our sampling_period settings to define time
-				time.sleep(self.settings['sampling_period'])
-				
-				i += 1
-
-				if self.interrupt_measurement_called:
-					# Listen for interrupt_measurement_called flag.
-					# This is critical to do, if you don't the measurement will
-					# never stop.
-					# The interrupt button is a polite request to the 
-					# Measurement thread. We must periodically check for
-					# an interrupt request
-					break
-
-		finally:            
-			if self.settings['save_h5']:
-				# make sure to close the data file
-				self.h5file.close()
+		self._read_spectrometer()
+		self.save_array[:,1] = self.y
+		if self.ui.save_every_spec_checkBox.isChecked():
+			self.save_array[:,0] = self.spec.wavelengths()
+			np.savetxt(self.app.settings['save_dir']+"/"+self.app.settings['sample']+str(self.point_counter)+".txt", self.save_array, fmt = '%.5f', header = 'Wavelength (nm), Intensity (counts)', delimiter = ' ')
+			self.point_counter += 1
+			pg.QtGui.QApplication.processEvents()
 
 	def save_single_spec(self):
-			save_array = np.zeros(shape=(2048,2))
-			self._read_spectrometer()
-			save_array[:,1] = self.y
-			save_array[:,0] = self.spec.wavelengths()
+		save_array = np.zeros(shape=(2048,2))
+		self._read_spectrometer()
+		save_array[:,1] = self.y
+		save_array[:,0] = self.spec.wavelengths()
 
-			np.savetxt(self.settings['save_dir']+"/"+self.settings.sample+".txt", save_array, fmt = '%.5f', 
-					   header = 'Wavelength (nm), Intensity (counts)', delimiter = ' ')
+		np.savetxt(self.settings['save_dir']+"/"+self.settings.sample+".txt", save_array, fmt = '%.5f', 
+				   header = 'Wavelength (nm), Intensity (counts)', delimiter = ' ')
 
 	def _read_spectrometer(self):
 		if hasattr(self, 'spec'):
